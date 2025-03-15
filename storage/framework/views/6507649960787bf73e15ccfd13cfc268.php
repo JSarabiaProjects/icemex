c8d14d939082b98c19ec8783b3b2d8bbf5e463e9
JSarabiaProjects <3iff3l4rt@gmail.com>
2025-03-14 23:30:04 -0600

    Add files via upload

---
+++ storage/framework/views/6507649960787bf73e15ccfd13cfc268.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/css/app.css'); ?>
+</head>
+<body>
+    <div id="app">
+    </div>
+    <?php echo app('Illuminate\Foundation\Vite')('resources/js/app.js'); ?>
+</body>
+
+</html><?php /**PATH C:\xampp\htdocs\icemexico\resources\views/vue.blade.php ENDPATH**/ ?>
